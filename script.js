f986c8241236bb11dba663e2f9dddae69c8689e8
SachinWarrier3423 <sachin.warrier22@st.niituniversity.in>
2025-01-21 17:08:55 +0530

    fix division by zero error

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ buttons.forEach((button) => {
         const value = button.textContent;
 
         if (value === '=') {
-            if (previousInput && operator) {
+            if (operator === '/' && currentInput === '0') {
+                result.value = 'Error';
+                currentInput = '';
+                previousInput = '';
+                operator = '';
+            } else if (previousInput && operator) {
                 currentInput = eval(`${previousInput} ${operator} ${currentInput}`);
                 result.value = currentInput;
                 previousInput = '';
                 operator = '';
             }
+        }
+        
         } else if (['+', '-', '*', '/'].includes(value)) {
             operator = value;
             previousInput = currentInput;
